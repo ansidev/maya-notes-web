0a52ffbc7ba838d667c0d284b74d09a062aecd2e
Le Minh Tri <ansidev@gmail.com>
2014-09-21 04:04:05 +0700

    Update Javascript for register form

--- html/bootstrap/js/register.js
+++ html/bootstrap/js/register.js
@@ -1,7 +1,43 @@
 window.onload = function() {
-    document.getElementsById('email').onfocus = show("email-tooltip");
+    document.getElementById('email').onfocus = show('email-tooltip');
+    document.getElementById('email').onblur = hide('email-tooltip');
+
+    document.getElementById('username').onfocus = show('username-tooltip');
+    document.getElementById('username').onblur = hide('username-tooltip');
+
+    document.getElementById('password').onfocus = show('password-tooltip');
+    document.getElementById('password').onblur = hide('password-tooltip');
+
+    document.getElementById('confirm-password').onfocus = show('confirm-password-tooltip');
+    document.getElementById('confirm-password').onblur = hide('confirm-password-tooltip');
+
+    document.getElementById('first-name').onfocus = show('first-name-tooltip');
+    document.getElementById('first-name').onblur = hide('first-name-tooltip');
+
+    document.getElementById('last-name').onfocus = show('last-name-tooltip');
+    document.getElementById('last-name').onblur = hide('last-name-tooltip');
+
+    document.getElementById('birthday').onfocus = show('birthday-tooltip');
+    document.getElementById('birthday').onblur = hide('birthday-tooltip');
+
+    document.getElementById('male').onfocus = show('gender-tooltip');
+    document.getElementById('male').onblur = hide('gender-tooltip');
+
+    document.getElementById('female').onfocus = show('gender-tooltip');
+    document.getElementById('female').onblur = hide('gender-tooltip');
+
+    document.getElementById('tos-checkbox').onfocus = show('tos-checkbox-tooltip');
+    document.getElementById('tos-checkbox').onblur = hide('tos-checkbox-tooltip');
 }
 
 function show(element) {
-    document.getElementById(element).style.display = "inline";
+	return function() {
+    	document.getElementById(element).style.display = "inline";
+	}
+}
+
+function hide(element) {
+	return function() {
+    	document.getElementById(element).style.display = "none";
+    }
 }
